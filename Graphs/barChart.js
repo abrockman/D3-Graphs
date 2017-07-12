94b5a0a82535f521a736e6d5b58fc04d29519aa6
Andrew <a.brockman@rgu.ac.uk>
2017-07-12 21:25:28 +0100

    Update structure and config object added.

--- Graphs/barChart.js
+++ Graphs/barChart.js
@@ -2,63 +2,81 @@
 
 var barChart = function () {
 
-    //Convert to object
     /*Default Values*/
-    var lowerThreshold = 50;
-    var upperThreshold = 90;
-    var green = "rgb(12, 150, 30)";
-    var yellow = "rgb(255, 190, 0)";
-    var red = "rgb(255, 21, 21)";
+    var local = {
+        "width":900,
+        "height":500,
+        "barcolour":"black"
+    }
+
     var setHeight = 500;
     var setWidth = 900;
     var data = [];
     var svgContainer= "";
+    var svg = 0;
 
-    /*Initial Data, SVG object and Size*/
-    function createChartArea (svgContainer, data, height, width) {
-        /*If function is called with undefine values the defaults
-                    should not be overridden*/
-        this.setHeight = height;
-        this.setWidth = width;
-        this.svgContainer = svgContainer;
-        this.data = data;
+    //Only set values if they are provided
+    function configure (config) {
+        if(config != undefined){
+            var keyValues = Object.keys(config);
+            for(i=0;i<keyValues.length;i++){
+                if(config[keyValues[i]] != undefined){
+                    local[keyValues[i]] = config[keyValues[i]];
+                }
+            }
+        }else{
+            return "Configuration object is not valid.";
+        }
     }
 
-    /*Draw the graph*/
+    //Initial Data, SVG object and Size
+    function createChartArea (svgContainer, data, config) {
+        this.svgContainer = svgContainer;
+        this.data = data;
+        //Only set the
+        configure(config);
+    }
+
+    //Modifier for config values
+    function setConfig(config){
+        return configure(config);
+    }
+
+    //Sets all structural elements of the chart
     function drawChart () {
 
-        /*Add width to SVG */
-        var chart = d3.select(this.svgContainer)
-        .attr("width", this.setWidth)
-        .attr("height", this.setHeight);
+        //Dimensions for svg
+        svg = d3.select(this.svgContainer)
+            .attr("width", local.width)
+            .attr("height", local.height);
 
+        //Draw data
+        update(this.data);
+
+    }
+
+    /*Draws the data onto the chart*/
+    function update (data) {
+        console.log(svg)
         /*Ranges*/
-        var yRange = d3.scaleLinear().domain([0, d3.max(this.data)]).range([this.setHeight, 0]);
+        var yRange = d3.scaleLinear().domain([0, d3.max(data)]).range([local.height, 0]);
 
 
-        /*Bars*/
-        var barWidth = this.setWidth / this.data.length;
+        //Width of each bar
+        var barWidth = local.width / data.length;
 
-        var bars = chart.selectAll("g")
-        .data(this.data)
-        .enter()
-        .append("g")
-        .attr("transform", function(d, i){return "translate(" + i*barWidth + ", 0 )";});
+        //Join data
+        var bars = svg.selectAll("g")
+        .data(data);
 
-        bars.append("rect")
+        //Draw bars
+        bars.enter()
+            .append("rect")
             .attr("y", function(d){return yRange(d);})
+            .attr("x",function(d,i){return barWidth*i})
             .attr("height", function(d){return setHeight - yRange(d);})
             .attr("width", barWidth - 1)
-            .attr("fill", function(d){
-            if(d<lowerThreshold){
-                return red;
-            }else if(d>=lowerThreshold && d<upperThreshold){
-                return yellow;
-            }else{
-                return green;
-            }
-        });
-
+            .attr("fill", local.barcolour);
     }
 
     /*
@@ -67,6 +85,9 @@ var barChart = function () {
 
     return{
         init: createChartArea,
-        draw:drawChart
+        draw: drawChart,
+        setConfig: setConfig,
+        update:update,
+        local:local
     }
 }();
